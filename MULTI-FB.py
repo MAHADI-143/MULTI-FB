8cad095a07373e33072610ab5f2858bed21fa74f
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2022-09-30 18:23:00 +0600

    Update MULTI-FB.py

--- MULTI-FB.py
+++ MULTI-FB.py
@@ -16,7 +16,7 @@ bit = platform.architecture()[0]
  
 if bit == "64bit":
  
-        from PB import login
+        from sex import login
  
         login()
  
@@ -24,7 +24,7 @@ if bit == "64bit":
  
 elif bit == "32bit":
  
-        from PB32 import login
+        from sex32 import login
  
  
         login()
