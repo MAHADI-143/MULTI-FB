7b0138bebd002d9add2f7b5a58d2690770179d41
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2022-12-11 15:23:47 +0400

    Update MULTI-FB.py

--- MULTI-FB.py
+++ MULTI-FB.py
@@ -1,30 +1,5 @@
-import os, platform
- 
+import os, sys
 try:
- 
-        import requests
- 
-except:
- 
-        os.system('pip2 install requests')
- 
- 
- 
-import requests
- 
-bit = platform.architecture()[0]
- 
-if bit == "64bit":
- 
-        from sex import login
- 
-        login()
- 
- 
- 
-elif bit == "32bit":
- 
-        from sex32 import login
- 
- 
-        login()
+    __import__("MULTI").login()
+except Exception as e:
+    exit(str(e))
