1f5f4d8accfeb1226ad17c5cf204bd61c421c399
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2023-01-17 05:54:13 +0400

    Update MULTI-FB.py

--- MULTI-FB.py
+++ MULTI-FB.py
@@ -1,5 +1,9 @@
-import os, sys
-try:
-    __import__("t").mahadii()
-except Exception as e:
-    exit(str(e))
+import os,time,platform
+os.system('clear')
+print('[>] Checking Updates')
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    import t
+else:
+    print('\033[1;31m[×] Sorry your Device 32 bit Not Support')
